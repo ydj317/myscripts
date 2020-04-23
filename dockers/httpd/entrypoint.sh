51e294f258fcf35f98aab6dfb75bfa2db18c2a26
yongirl <yongirl320@163.com>
2020-04-23 17:02:13 +0800

    new

--- dockers/httpd/entrypoint.sh
+++ dockers/httpd/entrypoint.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
 
-
-if [ ! -f "/home/volume/httpd.conf" ]; then
-    cp /home/config/httpd.conf /home/volume/httpd.conf
-fi
-
 if [ ! -f "/home/volume/httpd-vhosts.conf" ]; then
-    cp /home/config/httpd-vhosts.conf /home/volume/httpd-vhosts.conf
+    cp /usr/local/apache2/conf/extra/httpd-vhosts.conf /home/volume/httpd-vhosts.conf
 fi
 
-rm -rf /usr/local/apache2/conf/httpd.conf
 rm -rf /usr/local/apache2/conf/extra/httpd-vhosts.conf
 
-
-cp /home/volume/httpd.conf /usr/local/apache2/conf/httpd.conf
 cp /home/volume/httpd-vhosts.conf /usr/local/apache2/conf/extra/httpd-vhosts.conf
+chmod 644 /usr/local/apache2/conf/extra/httpd-vhosts.conf
 
 httpd-foreground
